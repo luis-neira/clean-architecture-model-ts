3c386f2d110de3f0f52442be85eecb5933242e97
Luis Neira <luis.neira@lossexpress.com>
2023-01-26 02:47:02 -0500

    Remove old mappers

--- src/common/mappers/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as OrderMap } from './order.mapper';
-export { default as ProductMap } from './product.mapper';
-export { default as UserMap } from './user.mapper';
-export { default as ImageMap } from './image.mapper';
